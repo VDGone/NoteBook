ba763351ce6c42e64b29f54fdff662e19413a94a
VDGone <disasterpiece788@gmail.com>
2016-12-02 21:49:16 +0200

    Views for categories and create categories

--- server/config/routes.js
+++ server/config/routes.js
@@ -13,10 +13,11 @@ module.exports = function(app) {
     app.post("/login", auth.login);
     app.get("/logout", auth.logout);
 
-    app.get('/profile', auth.isAuthenticated, controllers.users.getProfile);
+    app.get("/profile", auth.isAuthenticated, controllers.users.getProfile);
 
-    app.get('/categories/create', auth.isAuthenticated, controllers.categories.getCreate);
-    app.post('/categories/create', auth.isAuthenticated, controllers.categories.postCreate);
+    app.get("/categories", controllers.categories.getPublic);
+    app.get("/categories/create", auth.isAuthenticated, controllers.categories.getCreate);
+    app.post("/categories/create", auth.isAuthenticated, controllers.categories.postCreate);
 
     app.get("/", function(req, res) {
         res.render("index");
