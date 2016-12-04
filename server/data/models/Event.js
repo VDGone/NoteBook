7057ba95979e52686a78f9294ae12542e6c107eb
Daniel Nikolov <horwits1@hotmail.com>
2016-12-04 19:41:09 +0200

    Added picture

--- server/data/models/Event.js
+++ server/data/models/Event.js
@@ -6,6 +6,7 @@ module.exports.init = () => {
     let eventSchema = mongoose.Schema({
         title: {type: String},
         creator: { type: String },
+        picture: { type: String },
         date: { type: Date, default: Date.now()},
         place: { type: String },
         description: { type: String }
