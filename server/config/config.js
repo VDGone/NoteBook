aa5536fd7910e3a3cfb96e102da1d1d9d8c2ffd0
VDGone <disasterpiece788@gmail.com>
2016-12-05 02:48:55 +0200

    Connection string

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,8 @@ const rootPath = path.normalize(path.join(__dirname, "/../../"));
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: "mongodb://localhost:27017/NoteIHit", // TODO: Change the name
+        //db: "mongodb://localhost:27017/NoteIHit", // TODO: Change the name
+        db: "mongodb://Atom:atom@ds113958.mlab.com:13958/notesihit",
         port: process.env.PORT || 3000
     }
 };
